b2eb51074acbd2e0d8f49d795245d99f210fc3d4
Gurjot Grewal <gurjot_grewal@outlook.com>
2021-12-24 18:20:05 -0500

    Made function to see empty spaces and print board

--- backtracking-algo.py
+++ backtracking-algo.py
@@ -1,4 +1,3 @@
-print('hello')
 board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
@@ -14,4 +13,23 @@ board = [
 def print_board(b):
     for i in range(len(b)):
         if i % 3 == 0 and i != 0:
-            print('- - - - - - - - - - - - - ')
+            print('——————————————————————')
+
+        for j in range(len(b[0])):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
+
+            if j == 8:
+                print(b[i][j])
+            else:
+                print(str(b[i][j]) + " ", end="")
+
+def empty_square(b):
+    for i in range(len(b)):
+        for j in range(len(b[0])):
+            if b[i][j] == 0:
+                return (i, j) #row,col
+                
+
+print_board(board)
+
