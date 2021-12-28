93778c81776b3a93cac0c4e570b0a0eb6a6b3984
Gurjot Grewal <gurjot_grewal@outlook.com>
2021-12-27 20:50:32 -0500

    Added valid number checker

--- backtracking-algo.py
+++ backtracking-algo.py
@@ -31,5 +31,35 @@ def empty_square(b):
                 return (i, j) #row,col
                 
 
-print_board(board)
 
+def valid_board(b,num,pos):
+    #check row
+    for i in range(len(b[0])):
+        if b[pos[0]][i] == num and pos[1] == i:
+            return False
+
+    #check col
+    for i in range(len(b)):
+        if b[pos[i]][pos[1]] == num and pos[0] == i:
+            return False
+
+    #check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3)
+            if b[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+def solve(b):
+    
+
+
+
+
+
+
+    
