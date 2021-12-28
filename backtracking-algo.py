42c6fda6497d80bf6d2f363395ffbcf98ad1adc8
Gurjot Grewal <gurjot_grewal@outlook.com>
2021-12-27 21:06:22 -0500

    implemented backtracking algo

--- backtracking-algo.py
+++ backtracking-algo.py
@@ -29,18 +29,19 @@ def empty_square(b):
         for j in range(len(b[0])):
             if b[i][j] == 0:
                 return (i, j) #row,col
+    return None
                 
 
 
 def valid_board(b,num,pos):
     #check row
     for i in range(len(b[0])):
-        if b[pos[0]][i] == num and pos[1] == i:
+        if b[pos[0]][i] == num and pos[1] != i:
             return False
 
     #check col
     for i in range(len(b)):
-        if b[pos[i]][pos[1]] == num and pos[0] == i:
+        if b[i][pos[1]] == num and pos[0] != i:
             return False
 
     #check box
@@ -48,13 +49,36 @@ def valid_board(b,num,pos):
     box_y = pos[0] // 3
 
     for i in range(box_y*3, box_y*3+3):
-        for j in range(box_x*3, box_x*3+3)
+        for j in range(box_x*3, box_x*3+3):
             if b[i][j] == num and (i,j) != pos:
                 return False
 
     return True
 
 def solve(b):
+
+    find = empty_square(b)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10): #loop 1-9
+        if valid_board(b, i, (row, col)): #check if num is valid
+            b[row][col] = i #add num to board
+
+            if solve(b):
+                return True
+
+            b[row][col] = 0
+
+    return False
+
+print_board(board)
+solve(board)
+print_board(board)
+
+
     
 
 
